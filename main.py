3a3df075b6541f33a236d5ebcd79c4b69aebf9ce
RoeserHe825 <RoeserHe825@northwested.org>
2024-09-09 14:18:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 print('Hello')
 print('Good Afternoon') 
+
+print('Hello world!')
+
+# Henry Roeser
+
+# 09 SEP 2024
+
+# Printing in Python
+
+# Create a variable to store a person's first name
+first_name = "henry"
+
+print(f'How are you doing today, {first_name}?')
+
+famous_person = 'Abraham Lincoln'
+
+quote = 'Bad promises are better broken than kept.'
+
+print(f'{famous_person} once said: {quote}')
+
+print(f'Abraham Lincoln once said: {quote}')
